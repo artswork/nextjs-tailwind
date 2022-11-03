d53373c01cd602735180f475f65bc953aae66642
artswork <3356331+artswork@users.noreply.github.com>
2022-11-03 21:34:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-tailwind
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-7ltwm6-b5tybw)
---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const about = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default about;
